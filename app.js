9edf36141f5f505e46c9c84662af328fc5282fee
Aiello <aim@GRUPOASOCIART.NET>
2021-11-09 16:23:04 -0300

    ignorando al node_modules

--- app.js
+++ app.js
@@ -1,67 +1,70 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const mysql = require('mysql');
-const port = 8000;
 
 const app = express();
+const port = process.env.PORT;
 
+//Middlewares
+app.use(express.static('public')); // contenido estático
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-//Contenido estático
-app.use(express.static('public'));
-// app.use('/assets', express.static (__dirname +'/public'));
-
-
-//Handlebars
+//Settings
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'hbs');
 
+//Partials
+hbs.registerPartials(__dirname +'/views/partials/');
+
+//Handlebars
 app.get('/', (req, res)=>{
     res.render('home',{
        titulo: 'AIM Service'
-    }
-    );
+    });
 });
 
 app.get('/productos',(req, res)=>{
     res.render('productos',{
-        titulo:'AIM Service: Nuestros Productos'
-    }
-    );
+        titulo:'AIM Service: Nuestros Productos',
+        encabezado: 'NUESTROS PRODUCTOS',
+        nombre:'Productos'
+    });
 });
 
 app.get('/servicios',(req, res)=>{
     res.render('servicios',{
-        titulo:'AIM Service: Nuestros Servicios'
-    }
-    );
+        titulo:'AIM Service: Nuestros Servicios',
+        encabezado: 'NUESTROS SERVICIOS',
+        nombre:'Servicios'
+    });
 });
 
 app.get('/formacion',(req, res)=>{
     res.render('formacion',{
-        titulo:'AIM Service: Formación'
-    }
-    );
+        titulo:'AIM Service: Formación',
+        encabezado: 'FORMACIÓN',
+        nombre:'Formación'
+    });
 });
 
 app.get('/contacto',(req, res)=>{
     res.render('contacto',{
-        titulo:'AIM Service: Contactanos'
-    }
-    );
+        titulo:'AIM Service: Contactanos',
+        encabezado: 'CONTACTO',
+        nombre:'contacto'
+    });
 });
 
 app.get('/nosotros',(req, res)=>{
     res.render('nosotros',{
-        titulo:'AIM Service: Quienes Somos'
-    }
-    );
+        titulo:'AIM Service: Quienes Somos',
+        encabezado: 'QUIENES SOMOS',
+        nombre:'Nosotros'
+    });
 });
-
-hbs.registerPartials(__dirname +'/views/partials/');
-
 
 
 //Conexión base de datos
@@ -75,13 +78,10 @@ const conn = mysql.createConnection({
 conn.connect((err)=>{
     if(err) throw err;
     console.log('Conexión establecida...');
-
 });
-
-
 
 
 //Server Listening
 app.listen(port, ()=>{
     console.log(`Server is running at port: ${port}`);
-})
+});
