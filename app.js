1b335d5f8d0c177f7a5804fd1cd548dd0d861fbf
Aiello <aim@GRUPOASOCIART.NET>
2021-11-15 21:42:07 -0300

    rutas al routes, nodemailer finalizado

--- app.js
+++ app.js
@@ -19,59 +19,6 @@ app.set('view engine', 'hbs');
 //Partials
 hbs.registerPartials(__dirname +'/views/partials/');
 
-//Handlebars
-app.get('/', (req, res)=>{
-    res.render('home',{
-       titulo: 'AIM Service'
-    });
-});
-
-app.get('/productos',(req, res)=>{
-    res.render('productos',{
-        titulo:'AIM Service: Nuestros Productos',
-        encabezado: 'NUESTROS PRODUCTOS',
-        nombre:'Productos'
-    });
-});
-
-app.get('/servicios',(req, res)=>{
-    res.render('servicios',{
-        titulo:'AIM Service: Nuestros Servicios',
-        encabezado: 'NUESTROS SERVICIOS',
-        nombre:'Servicios'
-    });
-});
-
-app.get('/formacion',(req, res)=>{
-    res.render('formacion',{
-        titulo:'AIM Service: Formación',
-        encabezado: 'FORMACIÓN',
-        nombre:'Formación'
-    });
-});
-
-app.get('/contacto',(req, res)=>{
-    res.render('contacto',{
-        titulo:'AIM Service: Contactanos',
-        encabezado: 'CONTACTO',
-        nombre:'contacto'
-    });
-});
-
-app.get('/nosotros',(req, res)=>{
-    res.render('nosotros',{
-        titulo:'AIM Service: Quienes Somos',
-        encabezado: 'QUIENES SOMOS',
-        nombre:'Nosotros'
-    });
-});
-app.get('/email-enviado',(req, res)=>{
-    res.render('enviado',{
-        titulo:'AIM Service',
-    });
-});
-
-
 
 //Server Listening
 app.listen(port, ()=>{
--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'aim-service'
+    database: 'aiello_maximiliano'
 });
 
 conn.connect((err) => {
@@ -17,23 +17,80 @@ conn.connect((err) => {
     console.log('Conexión establecida...');
 });
 
+// Select
+router.get('/productos', (req, res) => {
+    let sql = "SELECT * FROM aimproductos";
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.render('productos', {
+            results: results,
+            titulo:'AIM Service: Nuestros Productos',
+            encabezado: 'NUESTROS PRODUCTOS',
+            nombre:'Productos'
+        });
+    });
+}); 
+
+//Handlebars
+router.get('/', (req, res)=>{
+    res.render('home',{
+       titulo: 'AIM Service'
+    });
+});
+
+router.get('/servicios',(req, res)=>{
+    res.render('servicios',{
+        titulo:'AIM Service: Nuestros Servicios',
+        encabezado: 'NUESTROS SERVICIOS',
+        nombre:'Servicios'
+    });
+});
+
+router.get('/formacion',(req, res)=>{
+    res.render('formacion',{
+        titulo:'AIM Service: Formación',
+        encabezado: 'FORMACIÓN',
+        nombre:'Formación'
+    });
+});
+
+router.get('/contacto',(req, res)=>{
+    res.render('contacto',{
+        titulo:'AIM Service: Contactanos',
+        encabezado: 'CONTACTO',
+        nombre:'contacto'
+    });
+});
+
+router.get('/nosotros',(req, res)=>{
+    res.render('nosotros',{
+        titulo:'AIM Service: Quienes Somos',
+        encabezado: 'QUIENES SOMOS',
+        nombre:'Nosotros'
+    });
+});
+
+
 // Nodemailer
 router.post('/send-email', (req, res) => {
+    const nombre = req.body.nombre;
+    const correo = req.body.correo;
+    const mensaje = req.body.mensaje;
+
     const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false,
+        host: "smtp.mailtrap.io",
+        port: 2525,
         auth: {
-            user: 'benton.kling24@ethereal.email',
-            pass: 'xdzZyUAwxh8ZttVE2Q'
+          user: "e23a2b531ccf5a",
+          pass: "f28a21fbd4b4c4"
         }
-    });
+      });
 
     const mailOptions = {
-        from: 'fulano',
-        to: 'benton.kling24@ethereal.email',
-        subject: 'testeando',
-        text: 'hola que tal, ',
+        from: `${correo}`,
+        to: 'naranjaspintadas@gmail.com',
+        subject: `${nombre} a través de AIM-Service.com`,
+        text: `${mensaje}`,
     }
 
     transporter.sendMail(mailOptions, (error, info) => {
@@ -41,10 +98,12 @@ router.post('/send-email', (req, res) => {
             res.status(500).send(error.menssage);
         } else {
             console.log('Email enviado');
+            res.render('enviado', {
+                titulo: 'Gracias por contactarnos',
+                nombre: 'Consulta enviada'
+            });
             res.status(200).jsonp(reqbody);
-            
         }
-    res.redirect('/email-enviado');
     });
 });
 
